54a89180f5823acd31d819939e4d55190f5702b6
Aime Cesaire <mu.aimecesaire@gmail.com>
2024-10-28 19:46:34 -0400

    Navbar complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,20 @@
  * @license Apache-2.0
  */
 
+// Node modules
+import { useState } from 'react'
+
 
 // Components
 import Navbar from "./Navbar"
 
+
+
  const Header = () => {
+
+    const [navOpen, setNavOpen] = useState(false)
+
+
   return (
     <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
             <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
@@ -17,7 +26,7 @@ import Navbar from "./Navbar"
                     className="logo"
                     >
                         <img className="logo-img"
-                        src = "public/images/logo.png"
+                        src = "/images/logo.png"
                         width = {70}
                         height = {70}
                         alt = "AC"
@@ -28,13 +37,15 @@ import Navbar from "./Navbar"
                 </h1>
 
                 <div className="relative md:justify-self-center">
-                    <button className="menu-btn md:hidden" onClick={null}>
-                        <span className="material-symbols-rounded">menu</span>
+                    <button className="menu-btn md:hidden" onClick={() => setNavOpen((prev) => !prev)}>
+                        <span className="material-symbols-rounded">
+                            {navOpen ? "close" : "menu"}
+                        </span>
                     </button>
 
-                    <Navbar />
+                    <Navbar navOpen={navOpen} />
                 </div>
-                <a href="#contact" className=""> Contact Me</a>
+                <a href="#contact" className="btn btn-secondary max-md:hidden md:justify-self-end"> Contact Me </a>
             </div>
 
         </header>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,35 @@
  */
 
 //Node modules
-import { useRef } from 'react'
+import { useRef, useEffect } from 'react'
+import PropTypes from 'prop-types'
 
 
-const Navbar = () => {
+const Navbar = ({ navOpen}) => {
     const lastActiveLink = useRef()
+    const activeBox = useRef()
+
+    const initActiveBox = () => {
+        activeBox.current.style.top = lastActiveLink.current.offsetTop + 'px'
+        activeBox.current.style.left = lastActiveLink.current.offsetLeft + 'px'
+        activeBox.current.style.width = lastActiveLink.current.offsetWidth + 'px'
+        activeBox.current.style.height = lastActiveLink.current.offsetHeight + 'px'
+        
+    }
+
+    useEffect(initActiveBox, [])
+    window.addEventListener('resize', initActiveBox)
+
+    const activeCurrentLink = (event) => {
+        lastActiveLink.current?.classList.remove('active')
+        event.target.classList.add('active')
+        lastActiveLink.current = event.target
+
+        activeBox.current.style.top = event.target.offsetTop + 'px'
+        activeBox.current.style.left = event.target.offsetLeft + 'px'
+        activeBox.current.style.width = event.target.offsetWidth + 'px'
+        activeBox.current.style.height = event.target.offsetHeight + 'px'
+    }
 
     const navItems = [
         {
@@ -36,16 +60,25 @@ const Navbar = () => {
     ]
 
   return (
-    <nav className="navbar">
+    <nav className={'navbar ' + (navOpen ? 'active' : '')}>
         {
             navItems.map(({ label, link, className, ref }, key) => (
-                <a href={link} className={className} key={key} ref={ref} onClick={null}> 
+                <a href={link} className={className} key={key} ref={ref} onClick={activeCurrentLink}> 
                 {label}
-                 </a> )
+                 </a> 
+                 )
             )
         }
+        <div
+        className='active-box'
+        ref={activeBox}
+        ></div>
     </nav>
   )
+}
+
+Navbar.propTypes = {
+    navOpen: PropTypes.bool.isRequired
 }
 
 export default Navbar
